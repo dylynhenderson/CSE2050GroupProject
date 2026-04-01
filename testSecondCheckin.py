1639231e2646a1976a23774b7a4400a9559b9b16
Electric-6 <martin.sam27800@gmail.com>
2026-04-01 10:26:55 -0400

    there are docstrings coming out of my butt. Also soon we should look at deleting all tests and writing new ones b/c of so much code refactor. Potentially one testing file for each class

--- testSecondCheckin.py
+++ testSecondCheckin.py
@@ -2,43 +2,51 @@ import unittest
 from university import University
 
 class TestSearchByID(unittest.TestCase):
+    """Tests to verify progress SM"""
     
     def setUp(self):
+        """Setup an object to test on SM"""
         u = University()
         self.obj = u
 
-    # Test: target is in the middle
+
     def test_found_middle(self):
+        """Test object at middle index SM"""
         data = ["STU001", "STU002", "STU003", "STU004", "STU005"]
         self.assertEqual(self.obj.search_by_id(data, "STU003", 0, len(data)-1), 2)
 
-    # Test: target is first element
+
     def test_found_first(self):
+        """Test id at index 0 SM"""
         data = ["STU001", "STU002", "STU003", "STU004", "STU005"]
         self.assertEqual(self.obj.search_by_id(data, "STU001", 0, len(data)-1), 0)
 
-    # Test: target is last element
+
     def test_found_last(self):
+        """Test  id at last index SM"""
         data = ["STU001", "STU002", "STU003", "STU004", "STU005"]
         self.assertEqual(self.obj.search_by_id(data, "STU005", 0, len(data)-1), 4)
 
-    # Test: target not in list
+    
     def test_not_found(self):
+        """Test id no in list SM"""
         data = ["STU001", "STU002", "STU003", "STU004", "STU005"]
         self.assertEqual(self.obj.search_by_id(data, "STU999", 0, len(data)-1), -1)
 
-    # Test: empty list
+
     def test_empty_list(self):
+        """Test behavior on an empty list SM"""
         data = []
         self.assertEqual(self.obj.search_by_id(data, "STU001", 0, len(data)-1), -1)
 
-    # Test: single element (found)
+
     def test_single_element_found(self):
+        """Test one element successful search SM"""
         data = ["STU001"]
         self.assertEqual(self.obj.search_by_id(data, "STU001", 0, len(data)-1), 0)
 
-    # Test: single element (not found)
     def test_single_element_not_found(self):
+        """Test one element, failed search SM"""
         data = ["STU001"]
         self.assertEqual(self.obj.search_by_id(data, "STU002", 0, len(data)-1), -1)
         
